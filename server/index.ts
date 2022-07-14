14466ae75a00477b665404d5a5ab765513975fe2
Omar <46730385+OmarAbdelazizMahmed@users.noreply.github.com>
2022-07-14 02:00:18 +0200

    Added type checking

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { db } from './datastore';
+import { createPostHandler, listPostsHandler } from './handlers/postHandler';
 
 const app = express();
 app.use(express.json());
@@ -12,18 +12,9 @@ const requestLoggerMiddleware: express.RequestHandler = (req, res, next) => {
 app.use(requestLoggerMiddleware);
 
 
-app.get('/posts', (req, res) => {
-    res.send({ posts: db.listPosts() });
-});
+app.get('/posts', listPostsHandler);
 
-app.post('/posts', (req, res) => {
-    const post = req.body;
-    post.id = Math.random().toString();
-    post.createdAt = new Date();
-    console.log(`Created post ${post.title}`);
-    const newPost = db.createPost(post);
-    res.send({ post: newPost });
-});
+app.post('/posts', createPostHandler);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
--- server/types.ts
+++ server/types.ts
@@ -1,3 +1,5 @@
+import { RequestHandler } from "express";
+
 export interface User {
     id: string,
     firstName: string,
@@ -28,3 +30,5 @@ export interface Comment {
     text: string,
     createdAt: Date,
 }
+
+export type ExpressHandler<req, res>  = RequestHandler<string, Partial<res>, Partial<req>, any>;   
